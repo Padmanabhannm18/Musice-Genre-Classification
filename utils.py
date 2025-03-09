4ffb58bf21b3dde506be371e5f559ab1ccabab08
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-09 11:27:05 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_mel_spectrogram(audio_path, n_mels=128, n_fft=2048, hop_length=512):
-    """Extracts Mel Spectrogram features from an audio file."""
-    y, sr = librosa.load(audio_path, sr=22050)
-    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)
-    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
-    return mel_spec_db
