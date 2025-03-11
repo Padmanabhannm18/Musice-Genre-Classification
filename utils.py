41033881f5533818f9264966448f71c7e39dafea
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-11 21:01:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
 import os
 
-# Function to get best performing model
-def get_best_model(model_performance):
-    best_model_name = max(model_performance, key=lambda name: model_performance[name][1])
-    best_model, best_accuracy = model_performance[best_model_name]
-    return best_model_name, best_model, best_accuracy
-
 # Function to save uploaded file
 def save_uploaded_file(uploaded_file, temp_audio_path="temp_audio.wav"):
     with open(temp_audio_path, "wb") as temp_file:
